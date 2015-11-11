1fa540e53261fb1d425a7b1e952876279f99c2ac
Thomas <sketchbang@gmail.com>
2015-11-11 08:58:30 -0600

    experimenting with git

--- app/main/views.py
+++ app/main/views.py
@@ -6,17 +6,24 @@ from ..database import db_session, cursor as c
 from ..models import Student, Faculty, Course, Department, Enrolled, Staff
 from forms import LoginForm
 
+
 @main.route("/", methods=['GET', 'POST'])
 def index():
-    form = LoginForm()       
+    form = LoginForm()
     if form.validate_on_submit():
-       session['id_no'] = form.id_no.data
-       if session.get('id_no') is not None:
-           flash('hello member')
-       return redirect(url_for('.index'))
-    return render_template('index.html', 
-                           form=form, 
-			   id_no=session.get('id_no'))
+        session['id_no'] = form.id_no.data
+        if session.get('id_no') is not None:
+            flash('hello member')
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form,
+                           id_no=session.get('id_no'))
+
+
+@main.route("/test")
+def test():
+    return "you are git'in now, son."
+
 
 @main.route("/dashboard")
 def dash():
@@ -26,23 +33,27 @@ def dash():
 @main.route("/courses")
 def courses():
     courses = db_session.query(Course).all()
-    return render_template("courses.html", courses=courses) 
+    return render_template("courses.html", courses=courses)
+
 
 @main.route("/staff", methods=['GET', 'POST'])
 def staff():
     c.execute("select * from students")
     students = c.fetchall()
-    return render_template("staff.html",  students=students, id_no=session.get('id_no'))
+    return render_template("staff.html", students=students, id_no=session.get('id_no'))
+
 
 @main.route("/departments")
 def departments():
     return "departments"
+
 
 @main.route("/api/<target>")
 def api(target):
     t = globals()[target.title()]
     l = table_to_dict(db_session.query(t).all())
     return jsonify({target: l})
+
 
 def table_to_dict(table):
     l = []
